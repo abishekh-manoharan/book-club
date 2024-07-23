9b697251e244cec5b0e0c52ecbfaa4222f72c2a6
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-07-23 13:52:10 -0400

    on register, create an associated User class

--- BookClubApi/Controllers/AuthController.cs
+++ BookClubApi/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 
+using BookClubApi.Data;
 using BookClubApi.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -8,9 +9,14 @@ namespace BookClubApi.Controllers;
 [Route("[controller]")]
 public class AuthController : ControllerBase {
     private UserManager<ApplicationUser> userManager;
+    private SignInManager<ApplicationUser> signInManager;
+    private BookClubContext dbContext;
+    
 
-    public AuthController(UserManager<ApplicationUser> userManager) {
+    public AuthController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, BookClubContext dbContext) {
         this.userManager = userManager;
+        this.signInManager = signInManager;
+        this.dbContext = dbContext;
     }
 
     // [HttpGet("users")]
@@ -18,14 +24,31 @@ public class AuthController : ControllerBase {
 
     // }
 
-    [HttpPost("createUser")]
-    public async Task<bool> CreateUser(ApplicationUser user, string password) {
-        var result = await userManager.CreateAsync(user, password);
-        return result.Succeeded;
+    [HttpPost("Register")]
+    public async Task<string> Register(User user, ApplicationUser appUser, string password) {
+        var result = await userManager.CreateAsync(appUser, password);
+        if(result.Succeeded) {            
+            // Create User class with the associated AspNetUserId 
+            user.AspnetusersId = appUser.Id;
+            dbContext.Users.Add(user);
+            dbContext.SaveChanges();
+            return appUser.Id;
+        }
+
+        string errors = "";
+        foreach (var errs in result.Errors) {
+            errors += errs.Description + "\n";
+        }
+        return errors;
     }
 
-    [HttpGet]
-    public string GetBody(string name, int id) {
-        return name + id;
+    [HttpGet("Users")]
+    public List<User> GetAllUsers(){
+        return dbContext.Users.ToList<User>();
     }
+    // [HttpGet]
+    // public string GetBody(User user, ApplicationUser appUser, string password)  {
+    //     return "Fname " + user.FName + " Lname " + user.LName + " AppUsername " + appUser.UserName+ " AppEmail " + appUser.Email+ " AppPhoneNumber " + appUser.PhoneNumber+ " AppUsername " + appUser.UserName;
+
+    // }
 }
