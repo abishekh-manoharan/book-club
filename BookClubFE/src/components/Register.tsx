dd28c38417be3c05c3c7df317fa9035433e82a6d
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-07-31 12:44:07 -0400

    displaying registration error messages

--- BookClubFE/src/components/Register.tsx
+++ BookClubFE/src/components/Register.tsx
@@ -17,7 +17,7 @@ function Register() {
         e.stopPropagation();
         // checking for validity - particularly requirement
         const registrationForm = document.querySelector(".registrationForm") as HTMLSelectElement;
-        if(!registrationForm.checkValidity()) {
+        if (!registrationForm.checkValidity()) {
             registrationForm.reportValidity();
             return;
         }
@@ -29,37 +29,140 @@ function Register() {
             Email: email,
             password: password
         }
-        
+
         AuthService.register(registrationData)
-            .then(res => console.log(res))
+            .then(res => {
+                // resetting error messages
+                const errorElement = document.querySelector(".PasswordTooShort");
+                if (!errorElement?.classList.contains("hidden")) { // display only if 
+                    errorElement!.classList.toggle('hidden');
+                }
+                const errorElement2 = document.querySelector(".PasswordRequiresNonAlphanumeric");
+                if (!errorElement2?.classList.contains("hidden")) { // display only if 
+                    errorElement2!.classList.toggle('hidden');
+                }
+                const errorElement3 = document.querySelector(".PasswordRequiresLower");
+                if (!errorElement3?.classList.contains("hidden")) { // display only if 
+                    errorElement3!.classList.toggle('hidden');
+                }
+                const errorElement4 = document.querySelector(".PasswordRequiresUpper");
+                if (!errorElement4?.classList.contains("hidden")) { // display only if 
+                    errorElement4!.classList.toggle('hidden');
+                }
+                const errorElement5 = document.querySelector(".PasswordRequiresDigit");
+                if (!errorElement5?.classList.contains("hidden")) { // display only if 
+                    errorElement5!.classList.toggle('hidden');
+                }
+                const errorElement6 = document.querySelector(".DuplicateUserName");
+                if (!errorElement6?.classList.contains("hidden")) { // display only if 
+                    errorElement6!.classList.toggle('hidden');
+                }
+                const errorElement7 = document.querySelector(".DuplicateEmail");
+                if (!errorElement7?.classList.contains("hidden")) { // display only if 
+                    errorElement7!.classList.toggle('hidden');
+                }
+
+                console.log(res)
+                
+                // handling successful registration
+                if (res[0] === 'succeeded') {
+                    const submissionSuccessElement = document.querySelector(".submission-success");
+                    submissionSuccessElement!.classList.toggle("hidden");
+                    return;
+                }
+
+                // handling failed registration
+                res.forEach((code: string) => {
+                    if (code === "PasswordTooShort") {
+                        // display error messages
+                        const errorElement = document.querySelector(".PasswordTooShort");
+                        if (errorElement?.classList.contains("hidden")) { // display only if 
+                            errorElement.classList.toggle('hidden');
+                        }
+                    }
+                    if (code === "PasswordRequiresNonAlphanumeric") {
+                        // display error messages
+                        const errorElement = document.querySelector(".PasswordRequiresNonAlphanumeric");
+                        if (errorElement?.classList.contains("hidden")) { // display only if 
+                            errorElement.classList.toggle('hidden');
+                        }
+                    }
+                    if (code === "PasswordRequiresLower") {
+                        // display error messages
+                        const errorElement = document.querySelector(".PasswordRequiresLower");
+                        if (errorElement?.classList.contains("hidden")) { // display only if 
+                            errorElement.classList.toggle('hidden');
+                        }
+                    }
+                    if (code === "PasswordRequiresUpper") {
+                        // display error messages
+                        const errorElement = document.querySelector(".PasswordRequiresUpper");
+                        if (errorElement?.classList.contains("hidden")) { // display only if 
+                            errorElement.classList.toggle('hidden');
+                        }
+                    }
+                    if (code === "PasswordRequiresDigit") {
+                        // display error messages
+                        const errorElement = document.querySelector(".PasswordRequiresDigit");
+                        if (errorElement?.classList.contains("hidden")) { // display only if 
+                            errorElement.classList.toggle('hidden');
+                        }
+                    }
+                    if (code === "DuplicateUserName") {
+                        // display error messages
+                        const errorElement = document.querySelector(".DuplicateUserName");
+                        if (errorElement?.classList.contains("hidden")) { // display only if 
+                            errorElement.classList.toggle('hidden');
+                        }
+                    }
+                    if (code === "DuplicateEmail") {
+                        // display error messages
+                        const errorElement = document.querySelector(".DuplicateEmail");
+                        if (errorElement?.classList.contains("hidden")) { // display only if 
+                            errorElement.classList.toggle('hidden');
+                        }
+                    }
+
+                }
+                );
+
+            })
             .catch(e => {
-                if(e instanceof AxiosError) {
+                if (e instanceof AxiosError) {
                     console.log(e.message);
                 }
             });
 
         console.log('submit button clicked')
     }
-    
+
     return (
         <div>
             <form className="form registrationForm">
                 <label htmlFor="Username">Username</label>
-                <input value="amano" name="Username" id="Username" onChange={(e) => {setUsername(e.target.value)}} required/><br/>
+                <input name="Username" id="Username" onChange={(e) => { setUsername(e.target.value) }} required /><br />
+                <p className="DuplicateUserName hidden" style={{ "color": "red" }}>Username is already taken.</p>
                 <label htmlFor="FName">First Name</label>
-                <input value="abi" name="Fname" id="Fname" onChange={(e) => {setFName(e.target.value)}} required/>
+                <input name="Fname" id="Fname" onChange={(e) => { setFName(e.target.value) }} required />
                 <label htmlFor="LName">Last Name</label>
-                <input value="mano" name="LName" id="LName" onChange={(e) => {setLName(e.target.value)}} required/><br/>
+                <input name="LName" id="LName" onChange={(e) => { setLName(e.target.value) }} required /><br />
                 <label htmlFor="Email">Email</label>
-                <input value="abi@gmaaail.com" name="Email" id="Email" onChange={(e) => {setEmail(e.target.value)}} required/><br/>
+                <input name="Email" id="Email" onChange={(e) => { setEmail(e.target.value) }} required /><br />
+                <p className="DuplicateEmail hidden" style={{ "color": "red" }}>Email is already taken.</p>
 
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" onChange={(e) => {setPassword(e.target.value)}} required/><br/>
+                <input type="password" name="password" id="password" onChange={(e) => { setPassword(e.target.value) }} required /><br />
+                <p className="PasswordTooShort hidden" style={{ "color": "red" }}>Passwords must be at least 8 characters.</p>
+                <p className="PasswordRequiresNonAlphanumeric hidden" style={{ "color": "red" }}>Passwords must have at least one non alphanumeric character.</p>
+                <p className="PasswordRequiresDigit hidden" style={{ "color": "red" }}>Passwords must have at least one digit ('0'-'9')</p>
+                <p className="PasswordRequiresLower hidden" style={{ "color": "red" }}>Passwords must have at least one lowercase ('a'-'z')</p>
+                <p className="PasswordRequiresUpper hidden" style={{ "color": "red" }}>Passwords must have at least one uppercase ('A'-'Z').</p>
                 {/* <label htmlFor="confirmPassword">Confirm Password</label>
                 <input type="password" name="confirmPassword" id="confirmPassword" onChange={(e) => {setConfirmPassword(e.target.value)}} required/><br/> */}
 
                 <button onClick={submitButtonHandler}>Sign Up</button>
             </form>
+            <p className="submission-success hidden" style={{ "color": "green" }}>Registration successful</p>
         </div>
     );
 }
