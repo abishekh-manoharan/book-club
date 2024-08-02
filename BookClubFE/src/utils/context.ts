de93925f115ac6ffaeb225a79663f83fbfda7f16
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-08-02 12:25:57 -0400

    add function that ensures context is defined when attempting to access it

--- BookClubFE/src/utils/context.ts
+++ BookClubFE/src/utils/context.ts
@@ -1,4 +1,14 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import { AuthContext } from "./types";
 
-export const authContext = createContext<AuthContext>({auth: undefined, setAuth: undefined});
+// AUTH CONTEXT
+// auth context creation with undefined default value
+export const authContext = createContext<AuthContext | undefined>(undefined);
+// function that returns the context after ensuring that the context isn't undefined 
+export const GetAuthContext = () => {
+    const auth = useContext(authContext);
+    if(auth !== undefined) {
+        return auth;
+    }
+    throw new Error('auth context value not defined');
+}
