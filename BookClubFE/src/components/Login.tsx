09d4750f60bbe593eea70259c685ee6bda069896
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-08-02 12:49:03 -0400

    login page shows login error and success message, updates auth state, navigates to home page on success

--- BookClubFE/src/components/Login.tsx
+++ BookClubFE/src/components/Login.tsx
@@ -1,16 +1,36 @@
 import { useState } from "react";
 import AuthService from "../services/auth";
+import { GetAuthContext } from "../utils/context";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+    const nav = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const auth = GetAuthContext();
 
     const loginSubmitClick = (e: React.SyntheticEvent) => {
         e.preventDefault();
         e.stopPropagation();
 
-        AuthService.login(email, password).then(res => console.log(res));
+        AuthService.login(email, password).then(res => {
+            if (res[0] === 'succeeded') {
+                // displaying success message
+                document.querySelector('.loginSuccess')?.classList.remove('hidden');
+                document.querySelector('.loginError')?.classList.add('hidden');
+                // updating the auth state using context
+                auth.setAuth(true);
+                nav('/');
+            } else {
+                document.querySelector('.loginSuccess')?.classList.add('hidden');
+                document.querySelector('.loginError')?.classList.remove('hidden');
+            }
+            
+        }
+        );
+        // HANDLING LOGIN RESPONSE
     }
+
     return (
         <div>
             <form className="form loginForm">
@@ -20,7 +40,8 @@ function Login() {
                 <label htmlFor="password">Password</label>
                 <input type="password" name="password" id="password" value={password} onChange={(e) => { setPassword(e.target.value) }} required /><br />
 
-                <p className="emailError hidden" style={{ "color": "red" }}>Email is already taken.</p>
+                <p className="loginError hidden" style={{ "color": "red" }}>Email or password incorrect</p>
+                <p className="loginSuccess hidden" style={{ "color": "green" }}>Login successful</p>
                 <button onClick={loginSubmitClick}>Submit</button>
             </form>
         </div>
