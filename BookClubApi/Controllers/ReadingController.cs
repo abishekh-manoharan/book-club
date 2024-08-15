d0969d122daa9a6d365d8b065aba7a39cb21bc36
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-08-15 13:48:10 -0400

    initiated reading controller

---
+++ BookClubApi/Controllers/ReadingController.cs
@@ -0,0 +1,19 @@
+using BookClubApi.Data;
+using BookClubApi.DTOs;
+using BookClubApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookClubApi.Controllers;
+
+[Route("[controller]")]
+public class ReadingController : ControllerBase {
+    private UserManager<ApplicationUser> userManager;
+    private BookClubContext dbContext;
+
+    public ReadingController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, BookClubContext dbContext)
+    {
+        this.userManager = userManager;
+        this.dbContext = dbContext;
+    }
+}
