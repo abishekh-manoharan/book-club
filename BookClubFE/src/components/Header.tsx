112b72edf07974aeccb49ed1a5e4b983b3f26532
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-08-21 16:06:19 -0400

    navbar horizontal styling

--- BookClubFE/src/components/Header.tsx
+++ BookClubFE/src/components/Header.tsx
@@ -14,19 +14,22 @@ function Header() {
     }
 
     return (
-        <nav>
-            <NavLink to="/">Home</NavLink> <br />
-            {!auth.auth ? <>
-                <NavLink to="register">Register</NavLink> <br />
-                <NavLink to="login">Login</NavLink>
-            </> : <></>}
-            {auth.auth ? <>
-                <NavLink to="createClub">Create Club</NavLink><br></br>
-                <NavLink to="joinedClubs">Joined Clubs</NavLink><br></br>
-                <a onClick={logoutClickHandler} href="">Logout</a>
-            </> : <></>}
+        <>
+
+            <nav className="header">
+                <NavLink to="/">Home</NavLink> <br />
+                {!auth.auth ? <>
+                    <NavLink to="register">Register</NavLink> <br />
+                    <NavLink to="login">Login</NavLink>
+                </> : <></>}
+                {auth.auth ? <>
+                    <NavLink to="createClub">Create Club</NavLink><br></br>
+                    <NavLink to="joinedClubs">Joined Clubs</NavLink><br></br>
+                    <a onClick={logoutClickHandler} href="">Logout</a>
+                </> : <></>}
+            </nav>
             <Outlet />
-        </nav>
+        </>
     );
 }
 
