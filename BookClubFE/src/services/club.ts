a5492a34b311a8b1fef838a86c55b4e9d4998b71
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-08-26 13:44:05 -0400

    club search service

--- BookClubFE/src/services/club.ts
+++ BookClubFE/src/services/club.ts
@@ -16,7 +16,20 @@ const getJoinedClubs = () => {
     return axios.get(BE_URL + "joinedClubs", { withCredentials: true }).then(res => res.data.$values);
 }
 
+const getSearchResults = (query: string) => {
+    // case where no value is entered
+    if(query === undefined ||query === null || query === "") {
+        return new Promise((_resolve, reject) => reject("no query value"));
+    }
+    
+    return axios.get(BE_URL + "getSearchResults/" + query, {
+        "data": {query: query},
+        ...options
+    }).then(res => res.data.$values);
+}
+
 export default {
     createClub,
-    getJoinedClubs
+    getJoinedClubs,
+    getSearchResults
 };
