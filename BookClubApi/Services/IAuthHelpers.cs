ccf2e6fa4f94f01ade179b56b062a60251f4edab
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-09-12 16:12:25 -0400

    update auth service to include function that indicates if a user is an admin of a specific club

--- BookClubApi/Services/IAuthHelpers.cs
+++ BookClubApi/Services/IAuthHelpers.cs
@@ -3,4 +3,5 @@ using BookClubApi.Models;
 namespace BookClubApi.Services;
 public interface IAuthHelpers {
     Task<User?> GetUserClassOfLoggedInUser(System.Security.Claims.ClaimsPrincipal User);    
+    Task<bool?> IsUserAdminOfClub(System.Security.Claims.ClaimsPrincipal User, int ClubId);
 }
