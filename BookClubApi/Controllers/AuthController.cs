eb6f1616fd4375a9d22eb56634e05c873716b0e9
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-10-17 13:38:57 -0400

    updates to the auth controller to improve robustness

--- BookClubApi/Controllers/AuthController.cs
+++ BookClubApi/Controllers/AuthController.cs
@@ -28,47 +28,55 @@ public class AuthController : ControllerBase
     // On success, returns an IEnumerable object containing the "success" keyword and the user's ID.
     // On failure, returns an IEnumberable object of error codes
     [HttpPost("Login")]
-    public async Task<IEnumerable<string>> Login(string email, string password)
+    public async Task<ActionResult<List<string>>> Login(string email, string password)
     {
-        // trying to find user with matching email
-        ApplicationUser? user = await userManager.FindByEmailAsync(email);
-        if (user != null)
+        if (ModelState.IsValid)
         {
-            // attempting password sign in
-            var result = await signInManager.PasswordSignInAsync(user!, password, true, false);
-            // return success message + user id if sign in was successful
-            if (result.Succeeded)
+            // trying to find user with matching email
+            ApplicationUser? user = await userManager.FindByEmailAsync(email);
+            if (user != null)
             {
-                return ["succeeded", user.Id];
+                // attempting password sign in
+                var result = await signInManager.PasswordSignInAsync(user!, password, true, false);
+                // return success message + user id if sign in was successful
+                if (result.Succeeded)
+                {
+                    return Ok(new List<string> { "succeeded", user.Id });
+                }
             }
+            // return error if user with email not found or if password sign in fails
+            return BadRequest(new List<string> { "error" });
         }
-        // return error if user with email not found or if password sign in fails
-        return ["error"];
+        return BadRequest(ModelState);
     }
 
     // Action method that takes in user registration data then attempts to creates a user.
     // On success, returns an IEnumerable object containing the "success" keyword and the user's ID.
     // On failure, returns an IEnumberable object of error codes
     [HttpPost("Register")]
-    public async Task<IEnumerable<string>> Register(User user, ApplicationUser appUser, string password)
+    public async Task<ActionResult<List<string>>> Register(User user, ApplicationUser appUser, string password)
     {
-        var result = await userManager.CreateAsync(appUser, password);
-        if (result.Succeeded)
+        if (ModelState.IsValid)
         {
-            // Create User class with the associated AspNetUserId 
-            user.AspnetusersId = appUser.Id;
-            dbContext.Users.Add(user);
-            dbContext.SaveChanges();
-            // return success message + user id if registration was successful
-            return ["succeeded", appUser.Id];
-        }
+            var result = await userManager.CreateAsync(appUser, password);
+            if (result.Succeeded)
+            {
+                // Create User class with the associated AspNetUserId 
+                user.AspnetusersId = appUser.Id;
+                dbContext.Users.Add(user);
+                dbContext.SaveChanges();
+                // return success message + user id if registration was successful
+                return Ok(new List<string> { "succeeded", appUser.Id });
+            }
 
-        List<string> errors = new List<string>();
-        foreach (var errs in result.Errors)
-        {
-            errors.Add(errs.Code);
+            List<string> errors = new List<string>();
+            foreach (var errs in result.Errors)
+            {
+                errors.Add(errs.Code);
+            }
+            return BadRequest(errors);
         }
-        return errors;
+        return BadRequest(ModelState);
     }
 
     // action method used to update a user's password
@@ -104,7 +112,7 @@ public class AuthController : ControllerBase
     [HttpGet("IsLoggedIn")]
     public ActionResult<bool> IsLoggedIn()
     {
-        return signInManager.IsSignedIn(this.User);
+        return Ok(signInManager.IsSignedIn(this.User));
     }
 
     // Action method that logs the user out, if they are logged in
@@ -137,11 +145,12 @@ public class AuthController : ControllerBase
             }
             return BadRequest(errors);
         }
-        return BadRequest("User not found.");        
+        return BadRequest("User not found.");
     }
 
     // Action method that returns the authenticated user's AspNetUserId
     [HttpGet("AspNetUserID")]
+    [Authorize]
     public string GetAspNetUserID()
     {
         return userManager.GetUserId(User)!;
@@ -149,6 +158,7 @@ public class AuthController : ControllerBase
 
     // Action method that returns the authenticated user's UserId
     [HttpGet("UserID")]
+    [Authorize]
     public int GetUserID()
     {
         var user = dbContext.Users
