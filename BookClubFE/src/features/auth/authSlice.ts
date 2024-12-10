42172d2c5f67c1301c0eb1794aa8c7bcb743f0a5
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-12-10 11:53:16 -0500

    implementation of login mutation endpoint

--- BookClubFE/src/features/auth/authSlice.ts
+++ BookClubFE/src/features/auth/authSlice.ts
@@ -1,19 +1,60 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import { apiSlice } from "../api/apiSlice";
+import { RootState } from "@/app/store";
 
+type LoginSuccess = boolean;
+type LoginError = {
+    [key: string]: string | string[]
+}
+type LoginResponse = LoginSuccess | LoginError;
+
+const initialState = {
+    isLoggedIn: false
+}
 
 const authSlice = createSlice({
     name: 'auth',
-    initialState: [],
+    initialState,
     reducers: {
-        login(state, action: PayloadAction<string>) {
-            console.log(state, action)
-        }
+        // loginStatusInitiated(state, action: PayloadAction<boolean>) {
+        //     state.isLoggedIn = action.payload; 
+        // }
     },
-    // extraReducers(builder) {
+    extraReducers(builder) {
+        builder.addMatcher(apiSliceWithAuth.endpoints.getStatus.matchFulfilled, (state, action) => {
+            state.isLoggedIn = action.payload;
+        })
+    }
+})
 
-    // }
+export const apiSliceWithAuth = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getStatus: builder.query<boolean, void>({
+            query: () => 'auth/isloggedin',
+            providesTags: [{ type: 'Auth', id: 'status' }]
+        }),
+        login: builder.mutation<LoginResponse, { email: string, password: string }>({
+            query: (info) => ({
+                url: 'auth/login',
+                method: 'POST',
+                body: info,
+                headers: {
+                    'Content-Type': 'x-www-form-urlencoded'
+                }
+            }),
+            invalidatesTags: () => ([{ type: 'Auth', id: 'status' }])
+        })
+    })
 });
 
-export const { login: loginAction } = authSlice.actions;
+// export const {  } = authSlice.actions;
+export const selectLoginStatus = (state: RootState) => state.auth.isLoggedIn;
 
 export default authSlice.reducer;
+
+
+export const { useGetStatusQuery, useLoginMutation } = apiSliceWithAuth;
+
+
+
+
