868c016dc0b11cd435a60653e2b198c2dfd09731
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-12-13 13:50:28 -0500

    updated header to use RTKQ getStatus query

--- BookClubFE/src/components/Header.tsx
+++ BookClubFE/src/components/Header.tsx
@@ -1,29 +1,32 @@
 import { NavLink, Outlet } from "react-router-dom";
-import { GetAuthContext } from "../utils/context";
-import AuthService from "../services/auth";
+// import { GetAuthContext } from "../utils/context";
+// import AuthService from "../services/auth";
 import SearchBar from "./SearchClubBar";
+import {  useGetStatusQuery } from "../features/auth/authSlice";
+// import { useAppSelector } from "../app/hooks";
 
 function Header() {
-    const auth = GetAuthContext();
+    // const auth = GetAuthContext();
+    const { data: loginStatus } = useGetStatusQuery();
 
-    const logoutClickHandler = (e: React.SyntheticEvent) => {
-        e.preventDefault();
-        e.stopPropagation();
+    // const logoutClickHandler = (e: React.SyntheticEvent) => {
+    //     e.preventDefault();
+    //     e.stopPropagation();
 
-        AuthService.logout();
-        auth.setAuth(false);
-    }
+    //     AuthService.logout();
+    //     auth.setAuth(false);
+    // }
 
     return (
         <>
 
             <nav className="header">
                 <NavLink to="/">Home</NavLink> <br />
-                {!auth.auth ? <>
+                {!loginStatus ? <>
                     <NavLink to="register">Register</NavLink> <br />
                     <NavLink to="login">Login</NavLink>
                 </> : <></>}
-                {auth.auth ? <>
+                {loginStatus ? <>
                     <NavLink to="createClub">Create Club</NavLink><br></br>
                     <NavLink to="joinedClubs">Joined Clubs</NavLink><br></br>
                     <a onClick={logoutClickHandler} href="">Logout</a>
