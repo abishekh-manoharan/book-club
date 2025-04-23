85bfe7c23373e518a9d868f49f709ed4051f63ee
abi-manoharan97 <abishekh.manoharan@gmail.com>
2025-04-23 13:06:19 -0400

    implemented an endpoint to create a notification for a single user

--- BookClubApi/Controllers/NotificationController.cs
+++ BookClubApi/Controllers/NotificationController.cs
@@ -36,15 +36,16 @@ public class NotificationController : ControllerBase
     // action method to get all of the logged in user's notifications
     [HttpGet("notifications")]
     [Authorize]
-    public async Task<ActionResult<UserDTO>> GetAllNotifications()
+    public async Task<ActionResult<List<NotificationDTO>>> GetAllNotifications()
     {
         User? user = await authHelpers.GetUserClassOfLoggedInUser(User);
         if (user != null)
         {
             var notifications = dbContext.Notification.Where(n => n.UserId == user.UserId).AsNoTracking();
             List<NotificationDTO> notificationDTOs = [];
-            
-            foreach (var n in notifications){
+
+            foreach (var n in notifications)
+            {
                 NotificationDTO notificationDTO = new(n.NotificationId, n.UserId, n.Text, n.Time, n.Link);
                 notificationDTOs.Add(notificationDTO);
             }
@@ -53,4 +54,54 @@ public class NotificationController : ControllerBase
         }
         return BadRequest("User instance associated with AspNetUser class not found.");
     }
+
+    // action method to create a notification record for a single user
+    [HttpPost("notificationForSingleUser")]
+    [Authorize]
+    public ActionResult<NotificationDTO> CreateNotificationForSingleUser([FromBody] CreateNotificationSingleUserValDTO notification)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                Notification newNotification = new()
+                {
+                    UserId = (int)notification.UserId!,
+                    Text = notification.Text,
+                    Link = notification.Link,
+                    Time = (DateTime)notification.Time!,
+                };
+
+                dbContext.Notification.Add(newNotification);
+                dbContext.SaveChanges();
+
+                return Ok();
+            }
+            catch (DbUpdateException dbe)
+            {
+                // if reading exists already, return 409 conflict status
+                if (dbe.InnerException!.Message.Contains("Duplicate"))
+                {
+                    return Conflict("Notification already exists.");
+                }
+                else if (dbe.InnerException!.Message.Contains("foreign key"))
+                {
+                    return NotFound("User not found with the associated id.");
+                } else {
+                    return BadRequest(dbe.InnerException!.Message);
+                }
+            }
+            catch (Exception e)
+            {
+                // handling all other errors when trying to save to db
+                return StatusCode(500, "Error saving the reading to the database. \n" + e.Message);
+            }
+
+        }
+        return BadRequest(ModelState);
+    }
+    // action method to create notification records for all club members
+
+    // action method to create notification records for all reading members
 }
+
---
+++ BookClubApi/DTOs/ValidationDTOs/Notifications/CreateNotificationSingleUserValDTO.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookClubApi.DTOs;
+
+public class CreateNotificationSingleUserValDTO {
+
+    [Required]
+    public int? UserId { get; set; }
+
+    public string Text { get; set; } = null!;
+
+    public string? Link { get; set; }
+
+    [Required]
+    public DateTime? Time { get; set; }
+}
