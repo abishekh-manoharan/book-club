47a82e246c21f26ff6a05a8347a6578f52eab541
abi-manoharan97 <abishekh.manoharan@gmail.com>
2025-07-08 12:35:55 -0400

    setting the progress bar width styling depending on the progress value

--- BookClubFE/src/features/reading/ActiveReadings/ProgressBar.tsx
+++ BookClubFE/src/features/reading/ActiveReadings/ProgressBar.tsx
@@ -2,23 +2,25 @@ import React, { useEffect, useState } from 'react';
 
 function ProgressBar({ progress, progressTotal, progresstypeId }: { progress: number | undefined, progressTotal: number | undefined, progresstypeId: number | undefined }) {
     const [width, setWidth] = useState(0);
+    const barStyle = {
+        width: `${width}%`
+    }
 
     useEffect(() => {
         if (progresstypeId === 1 || progresstypeId === 2 && progressTotal && progress) { // case where progress is measured in pages or chapters
             setWidth(progress! / progressTotal! * 100);
         }
+        else if (progresstypeId === 3 && progress) { // case where progress is measured in percentages
+            setWidth(progress);
+        }
     }, [setWidth, progress, progressTotal, progresstypeId]);
-    //  else if (progresstypeId === 2) { // case where progress is measured in chapters
 
-    // } else if ( progresstypeId === 3) { // case where progress is measured in percentages
-
-    // }
-
-return (
+    return (
         <div className='barContainer'>
-            <hr className='bar' />
+            <hr className='bar' style={barStyle} />
         </div>
     );
 }
 
 export default ProgressBar;
+
