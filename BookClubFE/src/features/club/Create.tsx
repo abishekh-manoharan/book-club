646718f3c3bf0dc409deb7b620974ed57644e567
abi-manoharan97 <abishekh.manoharan@gmail.com>
2025-08-25 12:47:40 -0400

    club creation page labels, inputs, and headers styling

--- BookClubFE/src/features/club/Create.tsx
+++ BookClubFE/src/features/club/Create.tsx
@@ -8,24 +8,25 @@ export type CreateClubFormData = {
     private: boolean
 }
 
-function Create() {
+function Create({ backLocation }: { backLocation: string }) { // the backLocation prop is used to indicate where the back button will lead
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
     const [profileImg, setProfileImg] = useState('');
     const [privateClub, setPrivateClub] = useState(false);
 
-    const [createClub, {isLoading, isSuccess}] = useCreateClubMutation();
+    const [createClub, { isLoading, isSuccess }] = useCreateClubMutation();
 
+    console.log(backLocation);
     const createButtonClickHandler = async (e: React.SyntheticEvent) => {
         e.preventDefault();
         // e.stopPropagation();
-    
+
         const form = document.querySelector(".form.clubCreationForm") as HTMLSelectElement;
-        if(!form.checkValidity()) {
+        if (!form.checkValidity()) {
             form.reportValidity();
             return;
         }
-        
+
         const privateCheckBox: HTMLInputElement | null = document.querySelector(".form.clubCreationForm .private");
         const clubFormData: CreateClubFormData = {
             name, description, profileImg, private: privateCheckBox!.checked
@@ -43,20 +44,24 @@ function Create() {
 
     return (
         <div>
-            Create Club
-
+            <div className="createClubHeading">
+                <h1>Create A Club</h1>
+                <p>Create a club and begin reading with your friends</p>
+            </div>
             <form className="form clubCreationForm">
                 <label htmlFor="Name">Name</label>
-                <input name="Name" id="Name" value={name} onChange={(e) => { setName(e.target.value) }} required /><br />
+                <input className="textInput" name="Name" id="Name" value={name} onChange={(e) => { setName(e.target.value) }} required /><br />
                 <label htmlFor="Description">Description</label>
-                <input name="Description" id="Description" value={description} onChange={(e) => { setDescription(e.target.value) }} /><br />
+                <input className="textInput" name="Description" id="Description" value={description} onChange={(e) => { setDescription(e.target.value) }} /><br />
                 <label htmlFor="ProfileImg">ProfileImg</label>
-                <input name="ProfileImg" id="ProfileImg" value={profileImg} onChange={(e) => { setProfileImg(e.target.value) }} /><br />
+                <input className="textInput" name="ProfileImg" id="ProfileImg" value={profileImg} onChange={(e) => { setProfileImg(e.target.value) }} /><br />
                 <label htmlFor="Private">Private</label>
-                <input name="Private" className="private" id="Private" type="checkbox" checked={privateClub} value="private" onChange={(e) => setPrivateClub(e.target.checked)}/>
+                <input className="private" name="Private" id="Private" type="checkbox" checked={privateClub} value="private" onChange={(e) => setPrivateClub(e.target.checked)} />
+
+                <br/>
                 <button onClick={createButtonClickHandler}>Create</button>
-                { isLoading && <>Loading</> }
-                { isSuccess && <>Success</> }
+                {isLoading && <>Loading</>}
+                {isSuccess && <>Success</>}
             </form>
         </div>
     );
