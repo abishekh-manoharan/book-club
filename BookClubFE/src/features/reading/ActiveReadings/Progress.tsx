636ff4738a186e2e0b4192129c00742ebb977b7b
abi-manoharan97 <abishekh.manoharan@gmail.com>
2026-01-08 13:20:50 -0500

    including prop for Progress component allowing the hiding of "update" features as needed in the application.

--- BookClubFE/src/features/reading/ActiveReadings/Progress.tsx
+++ BookClubFE/src/features/reading/ActiveReadings/Progress.tsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import UpdateReadingProgress from "../UpdateReadingProgress";
 import ProgressBar from "./ProgressBar";
 
-function Progress({progress, progresstypeId, progressTotal, clubId, bookId}: {progress: number, progressTotal: number | undefined, progresstypeId: number | undefined, bookId: number, clubId: number}) {
+function Progress({progress, progresstypeId, progressTotal, clubId, bookId, updateOption}: {progress: number, progressTotal: number | undefined, progresstypeId: number | undefined, bookId: number, clubId: number, updateOption?: boolean}) {
     const [modalShow, setModalShow] = useState(false);
     
     return (
         <div className="progress">
             {progress}
             <ProgressBar progress={progress} progresstypeId={progresstypeId} progressTotal={progressTotal}/>
-            <button className="button" onClick={() => setModalShow((state) => !state)}>Update Progress</button>
+            <button className="button" onClick={() => setModalShow((state) => !state)} hidden={updateOption}>Update Progress</button>
             {modalShow && <UpdateReadingProgress progress={progress} progresstypeId={progresstypeId} progressTotalProp={progressTotal} clubid={clubId} bookid={bookId} setModalShow={setModalShow}/>}
         </div>
     );
