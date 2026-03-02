2df54268e94fe9390720019e352d03181c6022ba
abi-manoharan97 <abishekh.manoharan@gmail.com>
2026-03-02 13:28:47 -0500

    re-organize location of getThreadsBatchQuery for main threads page

--- BookClubFE/src/features/discussion/DiscussionBoard.tsx
+++ BookClubFE/src/features/discussion/DiscussionBoard.tsx
@@ -1,19 +1,25 @@
 import { useParams } from "react-router-dom";
 import CreateThread from "./CreateThread";
-import { useGetThreadsQuery } from "./discussionSlice";
+// import { useGetThreadsQuery } from "./discussionSlice";
 import Threads from "./Threads";
+// import { useGetThreadsBatchQuery } from "./discussionSlice";
 
 function DiscussionBoard() {
     const { clubid, bookid } = useParams()
     const clubId = Number(clubid);
     const bookId = Number(bookid);
-
-    const {isError, isLoading} = useGetThreadsQuery({BookId: bookId, ClubId: clubId});
-    if(isError){
-        return "error";
-    } else if (isLoading){
-        return "loading";
-    }
+    // console.log(clubId)
+    // console.log(bookId)
+    // const {isError, error, isLoading} = useGetThreadsBatchQuery({BookId: bookId, ClubId: clubId, CursorThreadId: 0, CursorTimeAgo: new Date("2000-01-01T05:00:00.000Z").toISOString() });
+    // if(isError){
+    //     console.log(error)
+    //     return "error";
+    // } else if (isLoading){
+    //     return "loading";
+    // }
+    // else {
+    //     console.log("not error")
+    // }
     
     return (
         <div className="threadsFunctionsContainer">
--- BookClubFE/src/features/discussion/Threads.tsx
+++ BookClubFE/src/features/discussion/Threads.tsx
@@ -1,10 +1,22 @@
 import { useAppSelector } from "../../app/hooks";
-import { makeSelectNestedThreads } from "./discussionSlice";
+import { makeSelectNestedThreads, useGetThreadsBatchQuery } from "./discussionSlice";
 import Thread from "./Thread";
 
 function Threads({bookId, clubId}: {bookId: number, clubId: number}) {
-    const threads = useAppSelector(makeSelectNestedThreads({BookId: bookId, ClubId: clubId}));
+    const threads = useAppSelector(makeSelectNestedThreads({BookId: bookId, ClubId: clubId, CursorThreadId: 0, CursorTimeAgo: new Date("2000-01-01T05:00:00.000Z").toISOString() }));
+
+    const {isError, error, isLoading} = useGetThreadsBatchQuery({BookId: bookId, ClubId: clubId, CursorThreadId: 0, CursorTimeAgo: new Date("2000-01-01T05:00:00.000Z").toISOString() });
+        if(isError){
+            console.log(error)
+            return "error";
+        } else if (isLoading){
+            return "loading";
+        }
+        else {
+            console.log("not error")
+        }
     console.log(threads.rootThreads);
+    
     return (
         <div className="allThreads">
             {threads.rootThreads.map(thread => <Thread thread={thread} offset={0} reading={{ bookId, clubId }} depth={0}/>)}
