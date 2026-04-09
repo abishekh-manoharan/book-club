da64bd237db882239178e82d3c1e6e59dcb55a58
abi-manoharan97 <abishekh.manoharan@gmail.com>
2026-04-09 09:42:52 -0400

    close the modal if the user clicks outside of it

--- BookClubFE/src/features/discussion/JoinClubModal.tsx
+++ BookClubFE/src/features/discussion/JoinClubModal.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useRef } from "react";
+
 interface JoinClubModalProps {
     joinClubModalOpen: boolean,
     setJoinClubModalOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 function JoinClubModal({joinClubModalOpen, setJoinClubModalOpen}: JoinClubModalProps) {
+    const modal = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        // Close the modal if the user clicks outside of it
+        modal.current?.addEventListener("click", (e) => { 
+            if (e.target === modal.current) {
+                setJoinClubModalOpen(false);
+            }
+        });
+    }, [joinClubModalOpen]);
+
     return (
         <>
             {joinClubModalOpen == true ? 
-            <div className="modal">
+            <div ref={modal} className="modal">
                 <div className="deleteModalInner">
                     Join the club to take part in discussions.
                 </div>
