666b3a19494ceb478f30d4729034abdb7ef9749a
abi-manoharan97 <abishekh.manoharan@gmail.com>
2026-08-17 11:01:34 -0400

    initiate landing page

--- BookClubFE/src/components/LandingPage.tsx
+++ BookClubFE/src/components/LandingPage.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
 function LandingPage() {
+
     return (
-        <div>
-            Landing page
+        <div className="landingPage">
+            <div className="header">
+                <div className="leftSection">
+                    left
+                </div>
+                <div className="middleSection">
+                    <div className="headline">Reading is better <i><br/>together</i></div>
+                    <div className="subText">
+                        features featuresfeatures featuresfeatures featuresfeatures featuresfeatures features
+                    </div>
+                </div>
+                <div className="rightSection">
+                    right
+                </div>
+            </div>
         </div>
     );
 }
